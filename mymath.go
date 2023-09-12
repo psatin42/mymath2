6ee81b3f687f54c8450091f7dbd15969d1762671
psatin <ksyuha.93@mail.ru>
2023-09-12 20:11:45 +0300

    fix

--- mymath.go
+++ mymath.go
@@ -29,6 +29,15 @@ func Min(x, y float64) float64 {
 func Abs(x float64) float64 {
 	return math.Abs(x)
 }
+func Acos(x float64) float64 {
+	return math.Acos(x)
+}
+func Acosh(x float64) float64 {
+	return math.Acosh(x)
+}
+func Asin(x float64) float64 {
+	return math.Asin(x)
+}
 
 func Yn(n int, x float64) float64 {
 	return math.Yn(n, x)
